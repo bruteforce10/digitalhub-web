a6f9b71e81b4dc96b8d52874a60050228bd9cd14
Firdi Audi Putra <39665978+bruteforce10@users.noreply.github.com>
2024-07-24 10:45:25 +0700

    update bug v3

--- components/ButtonShimmer.jsx
+++ components/ButtonShimmer.jsx
@@ -20,7 +20,7 @@ export const ButtonShimmer = ({
       <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#ED1C77_0%,#5D4D87_50%,#ED1C77_100%)]" />
       <div
         className={cn(
-          `inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full  px-4 py-2 text-md font-medium text-white backdrop-blur-3xl bg-#2B032A
+          `inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full  px-4 py-2 text-md font-medium text-white backdrop-blur-3xl bg-[#2B032A]
           ${fontGrifter.className} tracking-[0.08em]`,
           classNameSecond
         )}
