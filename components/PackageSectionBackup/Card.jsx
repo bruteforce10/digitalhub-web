f8d0d709a37af312e5b7cb683e3307933c16ae94
Firdi Audi Putra <39665978+bruteforce10@users.noreply.github.com>
2024-08-01 17:35:08 +0700

    fix-data

--- components/PackageSectionBackup/Card.jsx
+++ components/PackageSectionBackup/Card.jsx
@@ -21,7 +21,7 @@ const Card = ({ title, description, price, list }) => {
         <h4 className="text-transparent text-xl font-medium tracking-tight bg-clip-text bg-gradient-to-r from-[#E96E4D] to-pink-500">
           Feature
         </h4>
-        <ul className="space-y-4 lg:min-h-[390px] min-h-[150px]">
+        <ul className="space-y-4 lg:min-h-[400px] min-h-[150px]">
           {list.map((list, index) => {
             return (
               <li key={index} className="flex gap-2 items-start font-medium">
@@ -37,9 +37,7 @@ const Card = ({ title, description, price, list }) => {
             );
           })}
         </ul>
-        <p className="text-sm opacity-60 leading-relaxed">
-          *untuk memberikan pengenalan dan konsep dasar untuk setiap modul
-        </p>
+        <p className="text-sm opacity-60 leading-relaxed">{description}</p>
       </div>
 
       <Dialog>
